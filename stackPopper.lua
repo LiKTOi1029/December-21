7ec1a46e5dabf1c78f49c782f36e5676169f2514
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-15 20:36:28 +0330

    recoding from scratch.

--- stackPopper.lua
+++ stackPopper.lua
@@ -1,23 +1,12 @@
-function fChecker()
-    local stack = {}
-    local choice = read()
-    for num = 1, #choice do
-        local character = choice:sub(num,num)
-        if character:match("[({[]") then
-            table.insert(stack,character)
-        elseif character:match("[)}]") then
-            if #stack == 0 then
-                return false
-            end
-            local top = table.remove(stack)
-            if (character == ")" and top ~= "(") or
-               (character == "}" and top ~= "{") or
-               (character == "]" and top ~= "[") then
-                return false
-            end
-        end
-    end
-    return #stack == 0
+function fChecker(input)
+	for num0 = 1, input:len(), 1 do
+		print(input:sub(num0, num0))
+	end
+	return true
 end
-
-print(fChecker())
+repeat
+	io.write(">>Type EXIT to exit and type anything else to input\n")
+	local choice = io.read("*l"):gsub("\n", "")
+	if choice ~= "EXIT" then fChecker(choice) 
+	else break end
+until true == false
