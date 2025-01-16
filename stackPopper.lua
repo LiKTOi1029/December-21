d4177d00c29b60e02148384099f1ba81e4942321
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-16 09:37:31 +0330

    finishing coding challenge stackPopper.lua

--- stackPopper.lua
+++ stackPopper.lua
@@ -5,13 +5,24 @@ function fChecker(input)
 		table.insert(tBrackets, input:sub(num0,num0))
 	end
 	for num1, bracket in ipairs(tBrackets) do
-		--thinking on how to count each single paranthesis, bracket, and curly brace rn
+	io.write(">>Checking "..bracket.."\n")
+		if (bracket == "}" and tPointer[1] == "{") or (bracket == "]" and tPointer[1] == "[") or (bracket == ")" and tPointer[1] == "(") then
+			io.write(">>Popping  "..tPointer[1].."\n")
+			table.remove(tPointer, 1)
+		elseif bracket ~= "}" or bracket ~= "]" or bracket ~= ")" then
+			table.insert(tPointer, 1, bracket)
+			io.write(">>Inserted "..bracket.."\n")
+		end
 	end
-	return true
+	if #tPointer == 0 then
+		return true
+	else
+		return false
+	end
 end
 repeat
 	io.write(">>Type EXIT to exit and type anything else to input\n")
 	local choice = io.read("*l"):gsub("\n", "")
-	if choice ~= "EXIT" then fChecker(choice) 
+	if choice ~= "EXIT" then io.write(tostring(fChecker(choice)).."\n")
 	else break end
 until true == false
